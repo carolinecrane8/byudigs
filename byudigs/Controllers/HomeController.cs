1e812114d0e138b2e43ab395e2365b972db79641
jamiescromar <jamiescromar@gmail.com>
2021-04-09 17:10:31 -0600

    Adding a Burial (half done)

--- byudigs/Controllers/HomeController.cs
+++ byudigs/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using byudigs.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,45 @@ namespace byudigs.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private byu_digsContext _context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, byu_digsContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+        //JAMIE's CONTROLLER PART
+
+        [HttpGet]
+        public IActionResult AddBurialSimple()
+        {
+            ViewBag.Plots = _context.Plot;
+            return View();
         }
 
+        [HttpPost]
+        public IActionResult AddBurialSimple(Plot p,int PlotInfo)
+        {
+            //int lowpairns = (int)p.LowPairNs;
+            //int highpairns = (int)p.HighPairNs;
+            //string buriallocationns = p.BurialLocationNs;
+            //string burialloactionew = p.BurialLocationEw;
+            //int lowpairew = (int)p.LowPairEw;
+            //int highpairew = (int)p.HighPairEw;
+            
+            return View("BurialList", _context.Plot);
+        }
+
+        public IActionResult BurialList()
+        {
+            return View(_context.Plot);
+        }
+        //public async Task<IActionResult> BurialList()
+        //{
+        //    return View(await _context.Plot.ToListAsync());
+        //}
+
+        //DONT TOUCH THIS SECTION
         public IActionResult Index()
         {
             return View();
