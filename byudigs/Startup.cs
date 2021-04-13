37be3197d18206c231b2a37e6b2f99dd8d476cfe
jamiescromar <jamiescromar@gmail.com>
2021-04-13 09:14:40 -0600

    changes

--- byudigs/Startup.cs
+++ byudigs/Startup.cs
@@ -50,15 +50,15 @@ namespace byudigs
                 .AddDefaultUI()
             .AddDefaultTokenProviders();
             services.AddControllersWithViews();
-            services.AddAuthentication()
-    .AddGoogle(options =>
-    {
-        IConfigurationSection googleAuthNSection =
-            Configuration.GetSection("Authentication:Google");
+    //        services.AddAuthentication()
+    //.AddGoogle(options =>
+    //{
+    //    IConfigurationSection googleAuthNSection =
+    //        Configuration.GetSection("Authentication:Google");
 
-        options.ClientId = googleAuthNSection["ClientId"];
-        options.ClientSecret = googleAuthNSection["ClientSecret"];
-    });
+    //    options.ClientId = googleAuthNSection["ClientId"];
+    //    options.ClientSecret = googleAuthNSection["ClientSecret"];
+    //});
             services.AddRazorPages();
         }
 
